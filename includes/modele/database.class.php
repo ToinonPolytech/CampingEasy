0bbbcd4fb3d7c91c69827dcbec165ee3afbbfbb2
Merwyyn <djebli.rayan@gmail.com>
2017-01-07 05:33:24 +0100

    no message

--- includes/modele/database.class.php
+++ includes/modele/database.class.php
@@ -1,4 +1,9 @@
 <?php
+	/**
+	TO DO : 
+		- Sécurité sur le nom des tables/valeurs update et select
+		- Sécurité sur les variables en argument (forcer les types)
+	**/
 	require("config.php");
 	class Database{
 		private $_db;
@@ -15,6 +20,22 @@
 				// En cas d'erreur précédemment, on affiche un message et on arrête tout
 				die('Erreur : '.$t->getMessage().' Numéro : '.$t->getCode());
 			}
+		}
+		function request($request, $array_where){
+			if (!empty($array_where) && !is_array($array_where))
+				return;
+			
+			foreach ($array_where as $key => $value)
+			{
+				if (strstr($request, "WHERE"))
+					$request.=" AND ";
+				else
+					$request.=" WHERE ";
+				
+				$request.=$key."=:".$key;
+			}
+			$this->_objectRequest=$db->prepare($request);
+			$this->_objectRequest->execute($array_where);
 		}
 		function select($name_table, $array_where, $array_select="*"){
 			if (is_array($array_select))
@@ -35,16 +56,25 @@
 			{
 				$request="SELECT *";
 			}
-			$request.=" FROM ".$name_table." WHERE ";
-			foreach ($array_where as $key => $value)
+			$request.=" FROM ".$name_table;
+			$this->request($request, $array_where);
+		}
+		function delete($name_table, $array_where){
+			$request="DELETE FROM ".$name_table;
+			$this->request($request, $array_where);
+		}
+		function update($name_table, $array_where, $array_update){
+			if (!is_array($array_update)) // On ne peut gérer que des tableaux pour la sécurité en PDO.
+				return;
+			
+			foreach ($array_update as $key => $value)
 			{
-				if (strstr($request, "WHERE"))
-					$request.=" AND ";
-				
-				$request.=$key."=:".$key;
+				if (!isset($request))
+					$request="UPDATE ".$name_table." SET ".$key."=:".$key;
+				else
+					$request.=",".$key."=:".$key;
 			}
-			$this->_objectRequest=$db->prepare($request);
-			$this->_objectRequest->execute($array_where);
+			$this->request($request, $array_where);
 		}
 		function fetch(){
 			return $this->_objectRequest->fetch();
