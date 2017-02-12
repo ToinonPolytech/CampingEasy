65c98bcbd8510e9bd5e7d8535ee558d39bb64ff7
ToinonPolytech <toinon.georget@etu.umontpellier.fr>
2017-02-12 20:03:46 +0100

    modif controller user

--- includes/controller/controllerFormulaire/user.controllerForm.php
+++ includes/controller/controllerFormulaire/user.controllerForm.php
@@ -10,12 +10,11 @@ require_once("../../modele/user.controller.class.php");
 
 $nom = htmlspecialchars($_POST['nom']);
 	$prenom = htmlspecialchars($_POST['prenom']); 
-	$code = htmlspecialchars($_POST['code']); //passe en formulaire ? je ne suis pas sur 
 	$droits = htmlspecialchars($_POST['droits']); 
 	$infosId = htmlspecialchars($_POST['infosId']);
 	$accessLevel = htmlspecialchars($_POST['accessLevel']);
 
-if(isset($nom) && isset($prenom) && isset($code) && isset($droits) && isset($infosId) && isset($accesLevel){
+if(isset($nom) && isset($prenom) && isset($droits) && isset($infosId) && isset($accesLevel){
 	//partie prototype : je ne sais pas comment on gère tout ça 
 	if($access_level == "client" ){
 		$client = new Client($infoId, $accessLevel, $droits, $nom, $prenom, $code);
