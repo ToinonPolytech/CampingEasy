c4ef61c2574cc662caf5512c74b27a765e652aaa
ToinonPolytech <toinon.georget@etu.umontpellier.fr>
2017-02-12 22:53:03 +0100

    nnn

--- includes/view/ajoutProblemeTechniqueForm.php
+++ includes/view/ajoutProblemeTechniqueForm.php
@@ -4,13 +4,16 @@
 <p>
 	Expliquez le problème que vous rencontrez   <br />
 	<textarea name="description" rows="6" cols="30"> 
-	</textarea> </br>
+	</textarea> <br/>
 	
 	Le problème se passe dans mon bungalow <br />
 	<input type="radio" name="isBungalow" value="true" id="oui" checked="checked" /> <label for="oui">Oui</label>
-	<input type="radio" name="isBungalow" value="false" id="non" /> <label for="non">Non</label>
+	<input type="radio" name="isBungalow" value="false" id="non" /> <label for="non">Non</label> <br/>
 	
-	Joindre une photo du problème 
+	
+	Joindre une photo du problème <br /> 
+	
+	<input type="submit" value="Signaler" />
 
 </p>
 </form>
