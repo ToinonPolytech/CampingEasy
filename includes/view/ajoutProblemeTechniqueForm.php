9e5e1c9ff4fb5623fa02316466de6588a28034fc
Merwyyn <djebli.rayan@gmail.com>
2017-02-20 21:01:43 +0100

    no message

--- includes/view/ajoutProblemeTechniqueForm.php
+++ includes/view/ajoutProblemeTechniqueForm.php
@@ -1,6 +1,6 @@
-<div class="col-lg-6" style="width:100%;" name="form-pbt" id="form-pbt">
+<div class="col-lg-6" style="width:100%;" name="formulaire-pbt" id="formulaire-pbt">
 	<span class="pull-left">Signaler un problème technique</span><br/>
-	<form role="form" method="post">
+	<form role="form" method="post" name="form-pbt" id="form-pbt">
 		<div class="form-group">
 			<label for="nom">Expliquez le problème que vous rencontrez</label><br/>
 			<textarea class="form-control" rows="6" cols="30" type="text" name="description" id="description"></textarea>
@@ -8,7 +8,7 @@
 			<input type="radio" name="isBungalow" value="true" id="oui" checked="checked" />Oui
 			<input type="radio"  name="isBungalow" value="false" id="non" />Non<br/>
 			
-			<button class="btn btn-success" onclick="loadTo('includes/controller/controllerFormulaire/problemeTechnique.controllerForm.php', {$('#form-pbt').serialize()}, '#form-pbt', 'prepend'); return false;">Signaler</button>
+			<button class="btn btn-success" onclick="loadTo('includes/controller/controllerFormulaire/problemeTechnique.controllerForm.php', $('#form-pbt').serialize(), '#formulaire-pbt', 'prepend'); return false;">Signaler</button>
 		</div>
 	</form>
 </div>
