38cac95758f0d2e771212d3ec2ea0b587844a2ce
Merwyyn <djebli.rayan@gmail.com>
2017-03-06 18:31:03 +0100

    Correction du code edit pbTech et de suppPbtech

--- includes/view/pbTechnique/modifierProblemeTechniqueForm.php
+++ includes/view/pbTechnique/modifierProblemeTechniqueForm.php
@@ -3,32 +3,42 @@
 		session_start();
 	
 	require_once($_SERVER['DOCUMENT_ROOT']."/includes/fonctions/general.php");
+	require_once(i("problemeTechnique.class.php"));
+	if(!auth() || !isset($_POST['id']))
+		exit();
+ 
+	$pb=new PbTech($_POST["id"]);
 	
-	if(isset($_POST['id']))
-	{
-		 
-		$db = new Database(); 
-		$db->select("problemes_technique", array('id' => $_POST['id'])); 
-		$pb=$db->fetch(); 
+	if ($pb->getIdUser()!=$_SESSION["id"])
+		exit();
 ?>
 
-		<div class="col-lg-6" style="width:100%;" name="formulaire-pbt" id="formulaire-pbt">
-			<span class="pull-left">Signaler un problème technique</span><br/>
-			<form role="form" method="post" name="form-pbt" id="form-pbt" enctype="multipart/form-data">
-				<div class="form-group">
-					<label for="nom">Expliquez le problème que vous rencontrez</label><br/>
-	<textarea class="form-control" rows="6" cols="30" type="text" name="description" id="description" value="<?php echo $pb['description']?> "></textarea>
-					<label for="isBungalow">Le problème se passe dans mon bungalow ?</label><br/>
-					<input type="radio" name="isBungalow" value="true" id="oui" checked="checked" />Oui
-					<input type="radio"  name="isBungalow" value="false" id="non" />Non<br/>
-					<label for="imageAjax" onclick="addImage();">Ajouter une photo</label><br/>
-					<button class="btn btn-success" onclick="loadTo('<?php echo str_replace($_SERVER['DOCUMENT_ROOT'], '', i('problemeTechnique.controllerForm.php')); ?>', '#form-pbt', '#formulaire-pbt', 'prepend', true); return false;">Signaler</button>
-				</div>
-			</form>
+<div class="col-lg-6" style="width:100%;" name="formulaire-pbt" id="formulaire-pbt">
+	<span class="pull-left">Signaler un problème technique</span><br/>
+	<form role="form" method="post" name="form-pbt" id="form-pbt" enctype="multipart/form-data">
+		<div class="form-group">
+			<label for="nom">Expliquez le problème que vous rencontrez</label><br/>
+			<textarea class="form-control" rows="6" cols="30" type="text" name="description" id="description" ><?php echo htmlspecialchars($pb->getDescription());?></textarea>
+			<label for="isBungalow">Le problème se passe dans mon bungalow ?</label><br/>
+			<input type="radio" name="isBungalow" value="true" id="oui" <?php if ($pb->getIsBungalow()) { echo "checked"; } ?> />Oui
+			<input type="radio"  name="isBungalow" value="false" id="non" <?php if (!$pb->getIsBungalow()) { echo "checked"; } ?> />Non<br/>
+			<label for="imageAjax" onclick="addImage();">Ajouter une photo</label><br/>
+			<?php
+				$i=0;
+				foreach ($pb->getPhotos() as $val)
+				{
+					if (!empty($val))
+					{
+						$i++;
+						?>
+						<script type="text/javascript">
+							addImage('<?php echo htmlentities($val); ?>');
+						</script>
+						<?php
+					}
+				}
+			?>
+			<button class="btn btn-success" onclick="loadTo('<?php echo str_replace($_SERVER['DOCUMENT_ROOT'], '', i('problemeTechnique.controllerForm.php')); ?>', '#form-pbt', '#formulaire-pbt', 'prepend', true); return false;">Signaler</button>
 		</div>
-<?php
-	}
-	else
-	{
-		echo 'pas de probleme reçu'; 
-	}
+	</form>
+</div>
