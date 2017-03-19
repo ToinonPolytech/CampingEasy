b48ced47c78653d899293e5ea7e72bba4fe290b1
Merwyyn <djebli.rayan@gmail.com>
2017-03-19 15:57:58 +0100

    no message

--- includes/view/equipe/classementEquipesView.php
+++ includes/view/equipe/classementEquipesView.php
@@ -3,10 +3,10 @@
 		session_start();
 		
 	require_once($_SERVER['DOCUMENT_ROOT']."/includes/fonctions/general.php");
-	
-		?><div class="col-lg-6" style="width:100%;" name="form-equipe" id="form-equipe"><a href="<?php echo str_replace($_SERVER['DOCUMENT_ROOT'], '', i('mesEquipesView.php')); ?>" class="pull-left">Voir mes équipes </a>
-	<?php
-		
+	?>
+<div class="col-lg-6" style="width:100%;" name="form-equipe" id="form-equipe">
+	<a href="<?php echo str_replace($_SERVER['DOCUMENT_ROOT'], '', i('mesEquipesView.php')); ?>" class="pull-left">Voir mes équipes</a>
+	<?php		
 		require_once(i("database.class.php"));
 		$db = new Database();
 		$db->setOrderCol("score");
@@ -16,25 +16,25 @@
 	<table class='table'>
 		<thead>
 			<tr>
-			  <th>Position</th>
-			  <th>Equipe</th>
-			   <th>Score </th>
+				<th>Position</th>
+				<th>Equipe</th>
+				<th>Score </th>
 			</tr>
 		</thead>
 		<tbody>
 		<?php
-		$i=0;
-		while($data=$db->fetch())
-		{ $i++; 
-			?>
-			<tr>
-				<td><?php echo $i; ?></td>
-				<td><?php echo $data['nom']; ?></td> 
-				<td><?php echo $data['score']; ?></td> 
-								
-			</tr>
-			<?php
-		}
+			$i=0;
+			while($data=$db->fetch())
+			{ 
+				$i++; 
+				?>
+				<tr>
+					<td><?php echo $i; ?></td>
+					<td><?php echo $data['nom']; ?></td> 
+					<td><?php echo $data['score']; ?></td> 				
+				</tr>
+				<?php
+			}
 		?>
 		</tbody>
 	</table> 
