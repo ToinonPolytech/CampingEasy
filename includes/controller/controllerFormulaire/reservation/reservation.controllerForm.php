67749adb9141b2ce62e56c72ad3789ee86b9930b
Merwyyn <djebli.rayan@gmail.com>
2017-03-21 19:53:38 +0100

    fix réservation

--- includes/controller/controllerFormulaire/reservation/reservation.controllerForm.php
+++ includes/controller/controllerFormulaire/reservation/reservation.controllerForm.php
@@ -1,9 +1,15 @@
 <?php 
+if (!isset($_SESSION))
+	session_start();
+
 require_once($_SERVER['DOCUMENT_ROOT']."/includes/fonctions/general.php");
 require_once(i("reservation.class.php"));
 require_once(i("reservation.controller.class.php"));
-echo 'controller';
-if (isset($_POST["id"]) && isset($_POST["type"]) && isset($_POST["idUser"]) && isset($_POST["idEquipe"]) && isset($_POST["nbrPersonnes"]))
+
+if (!auth())
+	exit();
+
+if (isset($_POST["id"]) && isset($_POST["type"]) && isset($_POST["idEquipe"]) && isset($_POST["nbrPersonnes"]))
 {
 	if ($_POST["type"]=="ACTIVITE")
 	{
@@ -13,17 +19,18 @@ if (isset($_POST["id"]) && isset($_POST["type"]) && isset($_POST["idUser"]) && i
 	else if (isset($_POST["time"]))
 	{
 		$time=$_POST["time"];
-		$reservation = new Reservation(NULL,htmlspecialchars($_POST["id"]), htmlspecialchars($_POST["type"]), htmlspecialchars($_POST["idUser"]), htmlspecialchars($_POST["idEquipe"]), htmlspecialchars($_POST["nbrPersonnes"]), $time);
-		$reservationController = new Controller_Reservation($reservation);
-		if ($reservationController->isGood())
-		{
-			$reservation->saveToDb();
-			echo 'Réservation effectuée'; 
-		}
 	}
 	else
 	{
 		echo "ERREUR : Un problème est survenu lors de l'envoi du formulaire.";
+		exit();
+	}
+	$reservation = new Reservation(NULL,htmlspecialchars($_POST["id"]), htmlspecialchars($_POST["type"]), $_SESSION["id"], htmlspecialchars($_POST["idEquipe"]), htmlspecialchars($_POST["nbrPersonnes"]), $time);
+	$reservationController = new Controller_Reservation($reservation);
+	if ($reservationController->isGood())
+	{
+		$reservation->saveToDb();
+		echo 'Réservation effectuée'; 
 	}
 }
 else
